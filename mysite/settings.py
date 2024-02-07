ec24dc3691f12b55045b094193b75e5a100bdacd
wor1bank <winfredmbithi91@gmail.com>
2024-02-07 16:51:13 +0300

    f

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[BASE_DIR/'static']
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
